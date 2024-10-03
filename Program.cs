bda46d4b8decae1b2bb41a209e813e718bc7ec08
Funclose <ar.doroxoff@gmail.com>
2024-10-03 18:06:54 +0300

    n

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LoadDataUser
+{
+    //доп задание в Test
+     class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            Test test = new Test();
+            
+
+            tasks.Add(test.GetArray(new string[] { "Artem", "Grigoriy", "Anna" }));
+            tasks.Add(test.GetArray(new string[] { "Elena", "Pavel", "Elena" })); 
+            tasks.Add(test.GetArray(new string[] { "Mikhail", "Ivan", "Olga" }));
+
+
+            await InisializeAsync();
+            try
+            {
+                User currentUser = await LoadUserDataAsync();
+                if (currentUser != null)
+                {
+                    
+                        Console.WriteLine($"ID {currentUser.Id}, Name: {currentUser.Name}, Email: {currentUser.Email}");
+                    
+                }
+            }
+            catch(OperationCanceledException)
+            {
+                Console.WriteLine("Запрос к серверу отменен, превышено время ожидания");
+            }
+        }
+
+
+
+
+        static async Task<User?> LoadUserDataAsync()
+        {
+            CancellationTokenSource canceltokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+            CancellationToken canceltoken = canceltokenSource.Token;
+            Task<User> loadTask = Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(3));
+                using (Applicationcontext context = new Applicationcontext())
+                {
+                    return  context.Users.FirstOrDefault()!;
+                }
+            });
+
+            if (await Task.WhenAny(loadTask, Task.Delay(TimeSpan.FromSeconds(10), canceltoken)) == loadTask)
+            {
+                return await loadTask;
+            }
+            else
+            {
+                throw new OperationCanceledException("Запрос к серверу был отменен");
+            }
+        }
+
+        static async Task InisializeAsync()
+        {
+            using (Applicationcontext db = new Applicationcontext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                db.Users.AddRange
+                    (
+                    new User[]
+                    {
+                    new User { Name = "Artem", Email = "something@gmail.com" },
+                    new User { Name = "Andrei", Email = "always@gmail.com" }
+                    });
+                db.SaveChanges();
+            }
+
+        }
+    }
+
+
+
+
+
+    public class Applicationcontext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-TBASQVJ;Database=testdb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+
+
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Azure.Core.HttpHeader;
+
+namespace LoadDataUser
+{
+     class Test
+    {
+         public async Task GetArray(string[] name)
+        {
+            await Task.Run(() =>
+            {
+                var dublicate = name.GroupBy(n => n).Where(c => c.Count() > 1).Select(c => c.Key).ToList();
+                if (dublicate.Count() > 0)
+                {
+                    throw new OperationCanceledException($"ошибка вывода, дублируеться: {string.Join(",", dublicate)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Имена: {string.Join(", ", name)}");
+                }
+            });
+        }
+    }
+}
